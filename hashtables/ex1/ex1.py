8af0522eecb1dfea4dd34244326abaa11316a17e
Shamskol <koladejun@yahoo.com>
2020-07-10 16:44:22 +0100

    Completed answering ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,17 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    ht = {}
+    for i in range(length):
+        weight = weights[i]
+        complement = limit - weight
+        if complement in ht:
+            return [i, ht[complement]]
+        else:
+            ht[weight] = i    
+
 
     return None
+
+weights_1 = [12, 6, 5, 7, 15, 20, 13, 21, 48, 24, 0]
+print(get_indices_of_item_weights(weights_1,11, 25 ))
