83cd5657c58d98c4cdc90c7946cc97da80229cf4
Shamskol <koladejun@yahoo.com>
2020-08-07 17:48:45 +0100

    Completed the code for ex4 as a stretch goal after meeting MVP

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,10 +2,19 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # Set up hash table
+    data = {}
+    # Set up result list
+    result = []
+    # Iterate through the nums
+    for i in a:
+        data[i] = i 
+        if  i !=0 and -i in data:
+            result.append(abs(i))
 
     return result
 
+    
 
 if __name__ == "__main__":
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
